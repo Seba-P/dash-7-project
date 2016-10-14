2fb0f644e83ec57e7825d02e6025e46753f6c339
Sebastian Pisklak <sebastian.pisklak@gmail.com>
2016-10-14 10:45:40 +0200

    stk3800 and efm32wg chips added

---
+++ apps/simple_leds/version.c
@@ -0,0 +1,2 @@
+const char _GIT_SHA1[] = "cb7acd";
+const char _APP_NAME[] = "simple_leds";
---
+++ stack/framework/hal/platforms/EFM32WG_STK3800/stk3800_main.c
@@ -0,0 +1,87 @@
+/* * OSS-7 - An opensource implementation of the DASH7 Alliance Protocol for ultra
+ * lowpower wireless sensor communication
+ *
+ * Copyright 2015 University of Antwerp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "scheduler.h"
+#include "bootstrap.h"
+#include "hwgpio.h"
+#include "hwuart.h"
+#include "hwleds.h"
+#include "hwlcd.h"
+#include "hwusb.h"
+#include "efm32wg_mcu.h"
+#include "hwdebug.h"
+#include "hwwatchdog.h"
+#include "platform.h"
+#include "userbutton.h"
+#include "platform_sensors.h"
+#include "em_gpio.h"
+#include <debug.h>
+
+
+#include "bsp_trace.h"
+
+#include "console.h"
+
+void __platform_init()
+{
+    __efm32wg_mcu_init();
+    __gpio_init();
+    __led_init();    // uses ports assigned to UART1 LOC3
+    __lcd_init();
+    console_init();
+
+#ifdef USE_CC1101
+    // configure the interrupt pins here, since hw_gpio_configure_pin() is MCU
+    // specific and not part of the common HAL API
+    hw_gpio_configure_pin(CC1101_GDO0_PIN, true, gpioModeInput, 0);
+    // hw_gpio_configure_pin(CC1101_SPI_PIN_CS, false, gpioModePushPull, 1);
+#endif
+    __hw_debug_init();
+
+    error_t err;
+    err = hw_gpio_configure_pin(BUTTON0, true, gpioModeInput, 0); assert(err == SUCCESS); // TODO pull up or pull down to prevent floating
+    err = hw_gpio_configure_pin(BUTTON1, true, gpioModeInput, 0); assert(err == SUCCESS); // TODO pull up or pull down to prevent floating
+
+    __watchdog_init(); // TODO configure from cmake?
+}
+
+void __platform_post_framework_init()
+{
+    __ubutton_init();
+	#ifdef PLATFORM_USE_USB_CDC
+
+    //Moved here to make sure LF XO is set in timer.
+    __usb_init_cdc();
+	#endif
+}
+
+int main()
+{
+	//BSP_TraceProfilerSetup();
+    // Only when using bootloader
+	//SCB->VTOR=0x4000;
+
+    //initialise the platform itself
+	__platform_init();
+    //do not initialise the scheduler, this is done by __framework_bootstrap()
+    __framework_bootstrap();
+    //initialise platform functionality that depends on the framework
+    __platform_post_framework_init();
+    scheduler_run();
+    return 0;
+}
